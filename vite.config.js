b621f5fcbd68916ec4800a3ea1be036b43054edc
otaviopato <otaviopato2017@outlook.com>
2023-02-17 20:53:53 -0300

    wip: ajustes do build

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/site-fronteira-tec-2",
+  base: "https://github.com/FronteiraTec/site-fronteira-tec-2.git",
   plugins: [react()]
 })
