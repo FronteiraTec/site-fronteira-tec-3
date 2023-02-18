3e8b6a89f4093f9a0da13d33d7321361dc39afa0
jeangondorek <jeangondorek73@gmail.com>
2023-02-18 10:25:36 -0300

    kashjas

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "https://github.com/FronteiraTec/site-fronteira-tec-2.git",
+  base: "https://github.com/FronteiraTec/site-fronteira-tec-3.git",
   plugins: [react()]
 })
